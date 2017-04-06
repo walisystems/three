f5150389c207871edb08107f45ff2169a1292f13
walisystems <info@walisystemsinc.com>
2017-04-06 16:19:10 -0400

    first push

--- Proj4/Form1.cs
+++ Proj4/Form1.cs
@@ -16,5 +16,10 @@ namespace Proj4
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("does it work now?");
+        }
     }
 }
